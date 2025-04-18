46b96a1c3300b302551d201bd0e8359dafb285b0
unicode1 <egoraleshin2006@gmail.com>
2025-04-18 17:35:18 +0300

    fixesd

--- OpenGrad/gamemodes/opengrad/gamemode/game/datamap_sv.lua
+++ OpenGrad/gamemodes/opengrad/gamemode/game/datamap_sv.lua
@@ -166,3 +166,26 @@ end}
 COMMANDS.points = {function(ply,args)
 	for i,point in pairs(SpawnPointsList) do ply:ChatPrint("	" .. point[1]) end
 end}
+
+concommand.Add("hg_points",function(ply)
+	for i,point in pairs(SpawnPointsList) do ply:ChatPrint("	" .. point[1]) end
+end)
+
+concommand.Add("hg_point",function(ply,cmd,args)
+	local name
+
+	for _name,info in pairs(SpawnPointsList) do
+		if info[1] == args[1] then name = _name break end
+	end
+
+	if not name then ply:ChatPrint("Ты еблан") return end
+
+	local tbl = ReadDataMap(name)
+	local point = {ply:GetPos() + Vector(0,0,5),Angle(0,ply:EyeAngles()[2],0),tonumber(args[2])}
+	table.insert(tbl,point)
+	WriteDataMap(name,tbl)
+
+	PrintMessage(3,"Точка " .. args[1])
+	SetupSpawnPointsList()
+	SendSpawnPoint()
+end)
